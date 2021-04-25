ef5e592cc398ddf8744875a49acd52a0f7556dbd
Shannon Young <shannon.ari1@gmail.com>
2021-04-25 15:16:18 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,11 +4,11 @@ class Anagram
     attr_accessor :word
 
     def initialize(word)
-        @word = word 
-    end 
+        @word = word
+    end
 
     def match(array)
         word_array = word.split('').sort
         array.select { |word| word.split('').sort == word_array}
-    end 
+    end
 end 
